185ad8cb10782cc300e559ff445a518f8ac2f8d6
Howard Chu <hyc@symas.com>
2022-05-03 16:09:18 +0100

    more

--- contrib/depends/hosts/darwin.mk
+++ contrib/depends/hosts/darwin.mk
@@ -1,5 +1,9 @@
+ifeq ($(host_arch),arm64)
+OSX_MIN_VERSION=11.0
+else
 OSX_MIN_VERSION=10.8
-OSX_SDK_VERSION=10.11
+endif
+OSX_SDK_VERSION=11.1
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=253.9
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -B $(host_prefix)/native/bin
