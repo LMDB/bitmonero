05f0960a018f4635a910d79bf486c46f2ab3c6c5
Howard Chu <hyc@symas.com>
2022-05-04 00:55:31 +0100

    more

--- contrib/depends/packages/native_cctools.mk
+++ contrib/depends/packages/native_cctools.mk
@@ -11,8 +11,8 @@ $(package)_dependencies=native_clang native_libtapi
 define $(package)_set_vars
 $(package)_config_opts=--target=$(host) --disable-lto-support --with-libtapi=$(host_prefix)
 $(package)_ldflags+=-Wl,-rpath=\\$$$$$$$$\$$$$$$$$ORIGIN/../lib
-$(package)_cc=$(host_prefix)/bin/clang
-$(package)_cxx=$(host_prefix)/bin/clang++
+$(package)_cc=$(host_prefix)/native/bin/clang
+$(package)_cxx=$(host_prefix)/native/bin/clang++
 endef
 
 # If clang gets updated to a version with a fix for https://reviews.llvm.org/D50559
--- contrib/depends/packages/native_libtapi.mk
+++ contrib/depends/packages/native_libtapi.mk
@@ -7,13 +7,10 @@ $(package)_sha256_hash=62e419c12d1c9fad67cc1cd523132bc00db050998337c734c15bc8d73
 $(package)_build_subdir=build
 $(package)_dependencies=native_clang
 
-define $(package)_set_vars
-$(package)_cc=$(host_prefix)/bin/clang
-$(package)_cxx=$(host_prefix)/bin/clang++
-endef
-
 define $(package)_config_cmds
   echo -n $(build_prefix) > INSTALLPREFIX; \
+  CC="$(host_prefix)/native/bin/clang" \
+  CXX="$(host_prefix)/native/bin/clang++" \
   cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix) \
     -DLLVM_INCLUDE_TESTS=OFF \
 	-DCMAKE_BUILD_TYPE=RELEASE \
