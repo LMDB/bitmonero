2b8374296e4c7705301af191f4ae95e67c2d5e15
Howard Chu <hyc@symas.com>
2022-05-04 04:51:17 +0100

    more

--- contrib/depends/hosts/darwin.mk
+++ contrib/depends/hosts/darwin.mk
@@ -2,9 +2,11 @@ OSX_MIN_VERSION=10.8
 OSX_SDK_VERSION=11.1
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=253.9
-darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -B $(host_prefix)/native/bin
-darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++ -B $(host_prefix)/native/bin
+darwin_CC=clang -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -B $(host_prefix)/native/bin
+darwin_CXX=clang++ -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++ -B $(host_prefix)/native/bin
 
+aarch64_darwin_CFLAGS=-target arm64-apple-darwin11
+x86_64_darwin_CFLAGS=-target $(host)
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
 darwin_ARFLAGS=cr
