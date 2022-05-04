2b11c6f3af0f820345d83c84cf02321b91f69d1e
Howard Chu <hyc@symas.com>
2022-05-04 01:15:03 +0100

    More

--- contrib/depends/packages/native_libtapi.mk
+++ contrib/depends/packages/native_libtapi.mk
@@ -5,12 +5,9 @@ $(package)_download_file=$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=62e419c12d1c9fad67cc1cd523132bc00db050998337c734c15bc8d73cc02b61
 $(package)_build_subdir=build
-$(package)_dependencies=native_clang
 
 define $(package)_config_cmds
   echo -n $(build_prefix) > INSTALLPREFIX; \
-  CC="$(host_prefix)/native/bin/clang" \
-  CXX="$(host_prefix)/native/bin/clang++" \
   cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix) \
     -DLLVM_INCLUDE_TESTS=OFF \
 	-DCMAKE_BUILD_TYPE=RELEASE \
@@ -21,7 +18,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) clangBasic && $(MAKE) libtapi
 endef
 
 define $(package)_stage_cmds
